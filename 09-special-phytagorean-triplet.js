12344fb2f00266f08b52e4ba5a2eb78be5e9c6da
sunyamare <sunyamare@gmail.com>
2019-12-31 12:59:51 +0100

    some more sad tries

--- 09-special-phytagorean-triplet.js
+++ 09-special-phytagorean-triplet.js
@@ -1,68 +1,55 @@
 /* A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 a2 + b2 = c2. For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
-There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc. */
-/*
-- begin iteration with: c = 1000 - b - a
-
-- c > b, otherwise break
-
-- b > a, otherwise break
-- a + b + c must be <= 1000
-- a ^ 2 + b ^ 2 must be c ^ 2
-
-... https://www.mathsisfun.com/numbers/pythagorean-triples.html
+There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc. 
+https://www.mathsisfun.com/numbers/pythagorean-triples.html:
+When m and n are any two positive integers (m > n):
+a = m2 − n2
+b = 2mn
+c = m2 + n2
+Then a, b, and c form a Pythagorean Triple.
+Example: m=2 and n=1
+a = 22 − 12 = 4 − 1 = 3
+b = 2 × 2 × 1 = 4
+c = 22 + 12 = 4 + 1 = 5
+Thus, we obtain the first Pythagorean Triple (3,4,5).
 */
-const findTriplet = sumOfSides => {
-    const maxSideC = 334; // sumOfSides - 3; // c > b > a -> 3, 2, 1 -> 2 + 1 are min. lenghts of a and b
-    const minSide = 1;
-    let stopVariable = 0;
-    let c = maxSideC;
-    for (c; c >= minSide; c--) {
-        if (stopVariable === 1) {
-            break;
-        }
-        console.log("c: " + c);
-        b = c - 1;
-        for (b; b >= minSide; b--) {
-            if (stopVariable === 1) {
-                break;
-            }
-            console.log("b: " + b);
-            if (c < b) {
-                console.log("c < b");
-                continue;
-            }
-            a = b - 1;
-            for (a; a >= minSide; a--) {
-                console.log("a: " + a);
-                if (a + b + c > sumOfSides) {
-                    console.log("a + b + c > " + sumOfSides);
-                    continue;
-                } else if (b < a) {
-                    console.log("b < a");
-                    continue;
-                } else if (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)) {
-                    console.log("***** 1 triple *****");
-                    console.log("a: " + a);
-                    console.log("b: " + b);
-                    console.log("c: " + c);
-                    continue;
-                } else if (
-                    a + b + c === 1000 &&
-                    Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)
-                ) {
-                    console.log("***** final triple *****");
-                    stopVariable = 1;
-                    break;
-                } else {
-                    continue;
-                }
+const findTheTriplet = () => {
+    const maxA = 350;
+    const maxB = 400;
+    const maxC = 450;
+    const maxM = 200;
+    const maxN = 200;
+    let tripletArray = [0, 0, 0];
+    console.log(tripletArray);
+    const tripletConstructor = (m, n) => {
+        tripletArray[0] = Math.pow(m, 2) - Math.pow(n, 2);
+        tripletArray[1] = 2 * m * n;
+        tripletArray[2] = Math.pow(m, 2) + Math.pow(n, 2);
+    };
+    loopOne: for (let n = 1; n < maxN; n++) {
+        loopTwo: for (let m = 1; m < maxM; m++) {
+            if (
+                tripletArray[0] < 0 ||
+                tripletArray[1] < 0 ||
+                tripletArray[2] < 0 ||
+                tripletArray[0] > tripletArray[1] ||
+                tripletArray[1] > tripletArray[2]
+            ) {
+                console.log("if (break): " + tripletArray);
+                break loopTwo;
+            } else if (
+                tripletArray[0] + tripletArray[1] + tripletArray[2] < 1000 &&
+                tripletArray[0] < maxA &&
+                tripletArray[1] < maxB &&
+                tripletArray[2] < maxC
+            ) {
+                console.log("else if: " + tripletArray);
+                tripletConstructor(m, n);
+                console.log(tripletArray);
+            } else {
+                console.log("else: " + tripletArray);
             }
         }
     }
-    console.log("a: " + a);
-    console.log("b: " + b);
-    console.log("c: " + c);
-    console.log("sum: " + (a + b + c));
 };
-findTriplet(1000);
+findTheTriplet();
